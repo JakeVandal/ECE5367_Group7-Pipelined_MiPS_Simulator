dbb90c28695df7c81d3879f8bc1122634b979296
JakeVandal <jake260van@gmail.com>
2026-04-14 16:16:41 -0500

    Add initial comments and include statement to main.c

---
+++ main.c
@@ -0,0 +1,7 @@
+// FIANL PROJECT
+// Group 7
+// ECE 5367
+// Jake Vandal, Nicholas Lloyd, Kenny Lam, Alejandro Castro, Hind Jaafar, Preston Hsu, Sebastian Padron
+// Pipelined MiPS Simulator
+
+#include <string.h>
